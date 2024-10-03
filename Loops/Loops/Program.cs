323fcc706b158a27b85021d376c191bf06d6c434
u1viii <dezzzgaming@gmail.com>
2024-10-03 13:27:07 +0400

    loops

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,41 @@
+﻿#region For
+//int sum = 0;
+//for(int i = 1; i < 6; i++)
+//{
+//    sum += i;
+//}
+//Console.WriteLine(sum);
+//for(int i = -5; true; i++)
+//{
+//    if (i % 6 == 0)
+//    {
+//        Console.WriteLine("Ədəd cütdür");
+//        break;
+//    }
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine("For bitdi");
+#endregion
+#region While, Do while
+//int i = 0;
+//for (; i < 10; i++)
+//{
+//    Console.WriteLine(i);
+//}
+//int num = 0;
+
+//Console.WriteLine("Adinizi daxil edin:");
+//string name = Console.ReadLine();
+//while (name.Any(x=> char.IsDigit(x)))
+//{
+//    Console.WriteLine("Adinizi daxil edin:");
+//    name = Console.ReadLine();
+//}
+string name = "";
+do
+{
+    Console.WriteLine("Adinizi daxil edin:");
+    name = Console.ReadLine();
+} while (name.Any(x => char.IsDigit(x)));
+Console.WriteLine("Adiniz " + name + " olaraq qeyd olundu.");
+#endregion
