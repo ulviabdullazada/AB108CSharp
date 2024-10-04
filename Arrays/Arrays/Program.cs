c05db1ec249e9c1163f5e544797c039a39ac6c50
u1viii <dezzzgaming@gmail.com>
2024-10-04 10:01:42 +0400

    arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,46 @@
+﻿//int[] numbers = { 2,3,4,5,9,2,3,4,7 };
+
+//for(int i = 0; i < numbers.Length; i++)
+//{
+//    Console.WriteLine(numbers[0]);
+
+//}
+
+
+//int length = int.Parse(Console.ReadLine());
+//int[] numbers = new int[length];
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    numbers[i] = int.Parse(Console.ReadLine());
+//}
+//Console.WriteLine("----------------------------------------------------------");
+//int sum = 0;
+//foreach (int number in numbers)
+//{
+//    sum += number;
+//}
+//Console.WriteLine(sum);
+
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    int number = numbers[i];
+//    sum += number;
+//}
+//Console.WriteLine(sum);
+
+
+int[,] numbers =
+{
+    {1, 2},
+    {4, 9},
+    {5, 10}
+};
+
+//for (int i = 0; i < numbers.GetLength(0); i++)
+//{
+//    for (int j = 0; j < numbers.GetLength(1); j++)
+//    {
+//        Console.WriteLine(numbers[i,j]);
+//    }
+//}
