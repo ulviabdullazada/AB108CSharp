5d77880d999cb52a59ab1cff9d54e21a520cdfce
u1viii <dezzzgaming@gmail.com>
2024-10-07 12:02:11 +0400

    big o

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -1,5 +1,5 @@
-﻿//int[] numbers = { 2,3,4,5,9,2,3,4,7 };
-
+﻿//int[] numbers = { 2,3,4 };
+//numbers[numbers.Length-1];
 //for(int i = 0; i < numbers.Length; i++)
 //{
 //    Console.WriteLine(numbers[0]);
@@ -30,12 +30,12 @@
 //Console.WriteLine(sum);
 
 
-int[,] numbers =
-{
-    {1, 2},
-    {4, 9},
-    {5, 10}
-};
+//int[,] numbers =
+//{
+//    {1, 2},
+//    {4, 9},
+//    {5, 10}
+//};
 
 //for (int i = 0; i < numbers.GetLength(0); i++)
 //{
@@ -44,3 +44,19 @@ int[,] numbers =
 //        Console.WriteLine(numbers[i,j]);
 //    }
 //}
+
+
+int[,] numbers =
+{
+    {2,4,6,8 }, //0,3
+    {1,3,5,7 }, //1,2
+    {1,2,3,5 }, //2,1
+    {4,6,9,10 } //3,0
+};
+int sum = 0;
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    //                  4          3
+    int j = numbers.GetLength(1) - i - 1;
+    Console.WriteLine(numbers[i,j]);
+}
---
+++ BigO/BigO/Program.cs
@@ -0,0 +1,63 @@
+﻿#region Linear search
+
+//string[] students = {
+//    "Ilgar",
+//    "Sabuhi",
+//    "Naciba",
+//    "Ruslan",
+//    "Selcan",
+//    "Rafiga"
+//};
+
+//for (int i = 0; i < students.Length; i++)
+//{
+//    Console.WriteLine(i+1 + ". addim");
+//    if (students[i] == "Rafiga")
+//    {
+//        Console.WriteLine("Element found at " + i + ". index");
+//        break;
+//    }
+//}
+#endregion
+
+#region Binary search
+int[] numbers = { 2, 4, 7, 11, 16, 22, 29, 35 };
+int leftI = 0;
+int rightI = numbers.Length;
+int search = -100;
+//Binary
+for (int i = 0; leftI < rightI; i++)
+{
+    Console.WriteLine(i+1 + ". addim");
+    int middleI = (leftI + rightI) / 2;
+    if (search == numbers[middleI])
+    {
+        Console.WriteLine("Element found at " + middleI + ". index");
+        break;
+    }
+    else if(leftI == rightI-1)
+    {
+        Console.WriteLine("Not found");
+        break;
+    }
+    else if (search > numbers[middleI])
+    {
+        leftI = middleI;
+    }
+    else
+    {
+        rightI = middleI;
+    }
+}
+Console.WriteLine("------------------------------------");
+//Linear
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(i + 1 + ". addim");
+    if (numbers[i] == search)
+    {
+        Console.WriteLine("Element found at " + i + ". index");
+        break;
+    }
+}
+#endregion
