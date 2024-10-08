450d2b05f17fba74fbef16fac9a925a5e7ee1f08
u1viii <dezzzgaming@gmail.com>
2024-10-08 12:18:17 +0400

    task 08 10 2024

---
+++ Task08102024/Task08102024/Program.cs
@@ -0,0 +1,24 @@
+﻿string text = "SaSSSalam Aleykumi. Allah sizi qorusun";
+string acharsoz = "SSalam";
+int keyI = 0;
+for (int i = 0; i < text.Length; i++)
+{
+    if (text[i] == acharsoz[keyI])
+    {
+        if (keyI == acharsoz.Length - 1)
+        {
+            Console.WriteLine(true);
+            break;
+        }
+        keyI++;
+    }
+    else
+    {
+        if(keyI != 0)
+        {
+            if (i > 0) i-=keyI;
+        }
+        keyI = 0;
+    }
+}
+Console.WriteLine(keyI);
